d511ac95e2849ed1160e2d990fe04029cd120769
Zhuo Lu <sethlu@mintkit.net>
2018-05-26 01:49:02 -0700

    :art: Attempt to fix styles

--- spec/fixtures/api/net-log/main.js
+++ spec/fixtures/api/net-log/main.js
@@ -2,7 +2,7 @@
 
 const {app, net} = require('electron')
 
-function request() {
+function request () {
   return new Promise((resolve) => {
     const req = net.request(process.env.TEST_URL)
     req.on('response', (resp) => {
@@ -12,7 +12,7 @@ function request() {
   })
 }
 
-function stopListening() {
+function stopListening () {
   return new Promise((resolve) => {
     console.log('before', net.isLogging)
     net.stopLogging(() => {
@@ -24,8 +24,9 @@ function stopListening() {
 
 app.on('ready', async () => {
   // Logging should start automatically if env TEST_DUMP_FILE_A not set
-  if (process.env.TEST_DUMP_FILE_A)
+  if (process.env.TEST_DUMP_FILE_A) {
     net.startLogging(process.env.TEST_DUMP_FILE_A)
+  }
 
   await request()
 
@@ -33,10 +34,11 @@ app.on('ready', async () => {
 
   // Optional second phase
   if (process.env.TEST_DUMP_FILE_B) {
-    if (process.env.TEST_DUMP_FILE_B === "--log-net-log")
+    if (process.env.TEST_DUMP_FILE_B === '--log-net-log') {
       net.startLogging()  // Log to file path specified by --log-net-log
-    else
+    } else {
       net.startLogging(process.env.TEST_DUMP_FILE_B)
+    }
 
     await request()
 
